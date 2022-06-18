ec64c9b013cd919db4f8c39eabaf440f45eba1fa
the-mattg <matemeo09@gmail.com>
2022-06-18 00:36:40 -0500

    New Weather App Home Info.

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,10 +1,12 @@
 const request = require('request')
 
+
+
 forecast = (longitude, latitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=d9ba4129c433a35536c21d58b1470b6a&query=' + latitude + ',' + longitude + '&units=f'
 
     request({ url, json: true }, (error, { body }) => {
-        const weather = ('It is ' + body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.'  )
+        const weather = ('It is ' + body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%. The current time is ' + body.current.observation_time + '.' )
         if (error) {
             callback('Unable to connect to weather service.')    
         } else if (body.error) {
